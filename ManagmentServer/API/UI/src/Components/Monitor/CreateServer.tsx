7e9de7cb75b77b2fc4a920ecddfc6d8b6d6dc455
dalibor <kun.dalibor@gmail.com>
2022-10-05 18:55:19 +0200

    Cleanup

--- ManagmentServer/API/UI/src/Components/Monitor/CreateServer.tsx
+++ ManagmentServer/API/UI/src/Components/Monitor/CreateServer.tsx
@@ -117,9 +117,7 @@ export default function AddNewServer(){
   return <form
     onSubmit={formik.handleSubmit}
     className="px-3 pb-2 w-full">
-
-      {/* <SelectList options={options} value={""} onChange={()=>{}} label="Type" /> */}
-
+      
       <FormSelect
         label="Type"
         id="type"
@@ -130,15 +128,6 @@ export default function AddNewServer(){
         <FormSelectOption value="US">MQTT</FormSelectOption>
         <FormSelectOption value="EF">OPC</FormSelectOption>
       </FormSelect>
-
-      {/* <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Select an option</label>
-      <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-        <option selected>Choose a country</option>
-        <option value="US">United States</option>
-        <option value="CA">Canada</option>
-        <option value="FR">France</option>
-        <option value="DE">Germany</option>
-      </select> */}
 
       <FormInput
         label="Name"
